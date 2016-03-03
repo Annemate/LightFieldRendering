1e8fbbb97994e1980fdd3bef0dfb9ade23ccd189
Annemate <bybub@h361.aau1x.klient5.nordkraft.site.aau.dk>
2016-03-03 16:39:10 +0100

    Grid spacing script GOGOGO

--- LightFieldRendering/Assets/gridSpacing.cs
+++ LightFieldRendering/Assets/gridSpacing.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class gridSpacing : MonoBehaviour {
 
-	// Use this for initialization
+	public static int cameraCounter;
+	private int cameraId;
+	public int camX = 14;
+	public int camY = 7;
+
+
+
 	void Start () {
-	
+		GameObject tmpCamera;
+		cameraId = cameraCounter;
+
+		print (cameraId);
+		cameraCounter++;
+
+
+		if (cameraId == 0) {
+
+			for(int i = 0; i <= camX; i++){
+				for(int j = 0; j <= camY; j++){
+					if(i != 0 || j != 0){ 
+						print (cameraCounter);
+						tmpCamera = Instantiate (gameObject, gameObject.transform.position + new Vector3((float) i, (float) j, 0), gameObject.transform.rotation) as GameObject;
+						print ("lol");
+						print (cameraCounter);
+						int tmp = (i*j) + i + j;
+						tmpCamera.name = "Camera " + tmp.ToString();
+
+						tmpCamera.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
+						if(i%2 != j%2){
+							tmpCamera.GetComponent<Camera>().backgroundColor = Color.white;
+						} else{
+							tmpCamera.GetComponent<Camera>().backgroundColor = Color.black;
+						}
+					}
+				}
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
